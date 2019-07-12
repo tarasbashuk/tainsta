28f793b437c427d8bf2ea740fa92311cdd8a17e5
Taras Bashuk <tarasbashuk@MacBook-Air-Taras.local>
2019-07-12 17:17:55 +0300

    initial commit

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const momgoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = User = momgoose.model('user', UserSchema)
